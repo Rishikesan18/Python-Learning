01e441f00eaf06964c3cbae62622c7ce1c3048c2
Rishikean18 <p.rishikesan18@gmail.com>
2024-11-03 18:54:40 +0530

    basic fun

---
+++ Functions/basic_fun.py
@@ -0,0 +1,9 @@
+def bas_fun():
+    print("This is basic function")
+
+bas_fun()
+
+def name_fun(name):
+    print(f"welcome {name}")
+    
+name_fun("rishi")
---
+++ Functions/fun_return.py
@@ -0,0 +1,8 @@
+def rishi(a,b):
+    return a+b
+
+res=rishi(15,6)
+print(res)
+
+st=rishi("rishi","keasan")
+print(st)
